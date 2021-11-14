3d49d220020abff8b352a0ef8d08a64fbe6b6196
JCjiechen <93553807+JCjiechen@users.noreply.github.com>
2021-11-14 13:39:33 -0500

    Update GameBattleDriver.java

--- src/GameBattleDriver.java
+++ src/GameBattleDriver.java
@@ -1,6 +1,11 @@
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * @author: Jie Chen, Jiaxi Wen, Choi LaiYun, Yilin Zhou
+ * This is the Driver class.
+ */
+
 //Game driver
 //Notice it's the only class declared as public.
 public class GameBattleDriver
